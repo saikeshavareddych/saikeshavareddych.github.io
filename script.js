1e276830110a6accfe46451792d9ae8398c2d6b6
saikeshavareddych <saikeshavareddy02@gmail.com>
2026-01-28 15:37:24 +0530

    updated the code github actions and projects

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("DevOps Portfolio Loaded 🚀");
+const text = "Sai Keshava — DevOps Engineer (2+ Years Experience)";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 80);
+  }
+}
+
+type();
